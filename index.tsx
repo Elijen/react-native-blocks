ad78ea3436be3cf775c0a19bd555833b4934027a
Petr Peller <petr.peller@gmail.com>
2019-11-09 20:47:10 +0100

    Added some TypeScript types, border props

--- index.tsx
+++ index.tsx
@@ -1,5 +1,62 @@
-import React, { useMemo } from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useMemo, FunctionComponent } from "react";
+import {
+  View,
+  StyleSheet,
+  StyleSheetProperties,
+  ViewStyle
+} from "react-native";
+import { isColor, isWidth, isBorderStyle } from "./utils";
+
+type SupportedTextStyleUnion =
+  | "color"
+  | "fontSize"
+  | "fontFamily"
+  | "lineHeight";
+
+type SupportedViewStyleUnion =
+  | "padding"
+  | "paddingTop"
+  | "paddingLeft"
+  | "paddingRight"
+  | "paddingBottom"
+  | "paddingVertical"
+  | "paddingHorizontal"
+  | "margin"
+  | "marginTop"
+  | "marginLeft"
+  | "marginRight"
+  | "marginBottom"
+  | "marginVertical"
+  | "marginHorizontal"
+  | "opacity"
+  | "flex"
+  | "height"
+  | "width"
+  | "minHeight"
+  | "maxHeight"
+  | "alignItems"
+  | "alignSelf"
+  | "justifyContent"
+  | "overflow";
+
+type SupportedViewStylesType = Pick<ViewStyle, SupportedViewStyleUnion>;
+
+type CustomProps = {
+  flex1: boolean;
+  flex2: boolean;
+  flex3: boolean;
+  row: boolean;
+  center: boolean;
+  centerHorizontal: boolean;
+  centerVertical: boolean;
+  bg: string;
+  size: number;
+  border: string | number | (string | number)[];
+  borderWidth: number;
+  borderColor: string;
+};
+
+interface BlockProps extends SupportedViewStylesType, Partial<CustomProps> {}
 
 export const withBlock = Component => props => {
   const { style, ...restProps } = props;
@@ -12,7 +69,7 @@ export const withBlock = Component => props => {
   return <Component {...restProps} style={[computedStyle.block, style]} />;
 };
 
-export const Block = withBlock(View);
+export const Block: FunctionComponent<BlockProps> = withBlock(View);
 
 const computeStyles = props => {
   let style = {};
@@ -37,6 +94,8 @@ const computeStyles = props => {
     }
   });
 
+  console.log(style);
+
   return StyleSheet.create({
     block: style
   });
@@ -59,6 +118,7 @@ const KEYS = [
   "marginHorizontal",
   "opacity",
   "color",
+  "flex",
   "fontSize",
   "fontFamily",
   "height",
@@ -72,6 +132,31 @@ const KEYS = [
 ];
 
 const ALIASES = { radius: "borderRadius", bg: "backgroundColor" };
+
+const createBorderProp = (prefix: BorderStylePrefixType) => props => {
+  let border = props.border;
+  if (!Array.isArray(props.border)) {
+    border = [border];
+  }
+
+  const style = {};
+  border.forEach(value => {
+    if (isColor(value)) {
+      style[prefix + "Color"] = value;
+    } else if (isWidth(value)) {
+      style[prefix + "Width"] = value;
+    } else if (isBorderStyle(value)) {
+      if (prefix !== "border") {
+        console.warn(
+          `Trying to set ${prefix}Style, but border style can only be set for the whole border (borderStyle)`
+        );
+      } else {
+        style[prefix + "Style"] = value;
+      }
+    }
+  });
+  return style;
+};
 
 const STYLES = {
   row: () => ({ flexDirection: "row" }),
@@ -106,5 +191,32 @@ const STYLES = {
     borderRightWidth: StyleSheet.hairlineWidth
   }),
   bgWhite: () => ({ backgroundColor: "#FFF" }),
-  bgGrey: () => ({ backgroundColor: "#f5f6f8" })
+  bgGrey: () => ({ backgroundColor: "#f5f6f8" }),
+  size: props => ({
+    width: props.size,
+    height: props.size
+  }),
+  border: createBorderProp("border"),
+  borderTop: createBorderProp("borderTop"),
+  borderBottom: createBorderProp("borderBottom"),
+  borderLeft: createBorderProp("borderLeft"),
+  borderRight: createBorderProp("borderRight")
 };
+
+type BorderStylePrefixType =
+  | "border"
+  | "borderTop"
+  | "borderBottom"
+  | "borderLeft"
+  | "borderRight";
+
+type BorderRadiusPrefixType =
+  | "border"
+  | "borderTopEnd"
+  | "borderTopStart"
+  | "borderTopLeft"
+  | "borderTopRight"
+  | "borderBottomEnd"
+  | "borderBottomStart"
+  | "borderBottomLeft"
+  | "borderBottomRight";
